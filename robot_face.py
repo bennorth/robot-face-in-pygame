d64392bf79d08f5f20f6ea35d2c547eb306acb68
Ben North <ben@redfrontdoor.org>
2014-10-07 21:44:12 +0100

    Up/down pupil movement.

--- robot_face.py
+++ robot_face.py
@@ -12,6 +12,7 @@ pupil_y = 10
 pupil_sz = 20
 
 pupil_dx = 0
+pupil_dy = 0
 
 while True:
     # Limit frame speed to 30 FPS
@@ -26,14 +27,24 @@ while True:
                 pupil_dx -= 2
             elif event.key == pygame.K_d:
                 pupil_dx += 2
+            elif event.key == pygame.K_w:
+                pupil_dy -= 2
+            elif event.key == pygame.K_s:
+                pupil_dy += 2
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 pupil_dx += 2
             elif event.key == pygame.K_d:
                 pupil_dx -= 2
+            elif event.key == pygame.K_w:
+                pupil_dy += 2
+            elif event.key == pygame.K_s:
+                pupil_dy -= 2
 
     pupil_x += pupil_dx
     pupil_x = min(25, max(5, pupil_x))
+    pupil_y += pupil_dy
+    pupil_y = min(25, max(5, pupil_y))
 
     sys.stdout.write('%d %d\r' % (pupil_x, pupil_y))
     sys.stdout.flush()
