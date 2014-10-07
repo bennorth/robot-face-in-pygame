77c15cb7b12eed1f6f053b6b955abf7804288a19
Ben North <ben@redfrontdoor.org>
2014-10-07 21:34:42 +0100

    Wrap drawing in event loop.
    See http://eli.thegreenplace.net/2008/12/13/writing-a-game-in-python-with-pygame-part-i/

--- robot_face.py
+++ robot_face.py
@@ -3,8 +3,25 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+finished = False
 
-pygame.draw.rect(screen, pygame.Color('red'), (50, 50, 50, 50))
-pygame.draw.rect(screen, pygame.Color('red'), (700, 50, 50, 50))
+while True:
+    # Limit frame speed to 30 FPS
+    #
+    time_passed = clock.tick(30)
 
-pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+    if finished:
+        break
+
+    # Redraw the background
+    screen.fill(pygame.Color(0, 0, 0, 255))
+
+    pygame.draw.rect(screen, pygame.Color('red'), (50, 50, 50, 50))
+    pygame.draw.rect(screen, pygame.Color('red'), (700, 50, 50, 50))
+
+    pygame.display.flip()
