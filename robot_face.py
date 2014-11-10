435a3d85e9b75d61f714a7f1a1fd32d5dfdf9624
Ben North <ben@redfrontdoor.org>
2014-11-10 20:08:33 +0000

    Update colours to match student's.

--- robot_face.py
+++ robot_face.py
@@ -63,11 +63,11 @@ while True:
         break
 
     # Redraw the background
-    screen.fill(pygame.Color(0, 0, 0, 255))
+    screen.fill(pygame.Color('grey'))
 
-    pygame.draw.rect(screen, pygame.Color('red'), (50, 50, 50, 50))
-    pygame.draw.rect(screen, pygame.Color('black'), (50 + eyes, 50 + eyes_up, pupil_sz, pupil_sz))
-    pygame.draw.rect(screen, pygame.Color('red'), (700, 50, 50, 50))
-    pygame.draw.rect(screen, pygame.Color('black'), (700 + eyes, 50 + eyes_up, pupil_sz, pupil_sz))
+    pygame.draw.rect(screen, pygame.Color('white'), (50, 50, 50, 50))
+    pygame.draw.rect(screen, pygame.Color('white'), (700, 50, 50, 50))
+    pygame.draw.rect(screen, pygame.Color('brown'), (700 + eyes, 50 + eyes_up, pupil_sz, pupil_sz))
+    pygame.draw.rect(screen, pygame.Color('brown'), (50 + eyes, 50 + eyes_up, pupil_sz, pupil_sz))
 
     pygame.display.flip()
